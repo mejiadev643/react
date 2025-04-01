175d10e8150d53f1d7a67444c327beb8b87902b0
cristian mejia <comejia@innovacion.gob.sv>
2025-04-01 09:00:42 -0600

    .

--- 06-tienda-y-carrito/src/components/Footer.jsx
+++ 06-tienda-y-carrito/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export function Footer () {
 
   return (
     <footer className='footer'>
-      <h4>Prueba técnica de React ⚛️ － <span>@midudev</span></h4>
+      <h4>Prueba técnica de React</h4>
       <h5>Shopping Cart con useContext & useReducer</h5>
     </footer>
   )
